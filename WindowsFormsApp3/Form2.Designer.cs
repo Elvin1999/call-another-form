4bde22a7812411c373673028ded41536795042a6
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-09 19:59:59 +0400

    Add project files.

---
+++ WindowsFormsApp3/Form2.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.okeybutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(332, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // okeybutton
+            // 
+            this.okeybutton.Location = new System.Drawing.Point(273, 203);
+            this.okeybutton.Name = "okeybutton";
+            this.okeybutton.Size = new System.Drawing.Size(194, 23);
+            this.okeybutton.TabIndex = 1;
+            this.okeybutton.Text = "okey";
+            this.okeybutton.UseVisualStyleBackColor = true;
+            this.okeybutton.Click += new System.EventHandler(this.okeybutton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.okeybutton);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okeybutton;
+    }
+}
---
+++ WindowsFormsApp3/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp3
+{
+    public partial class Login : Form
+    {
+        Form1 form1;
+        public Login(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+            okeybutton.BackColor = Color.Red;
+        }
+        private void okeybutton_Click(object sender, EventArgs e)
+        {
+            form1.BackColor = Color.Yellow;
+            Close();
+        }
+    }
+}
